db74c44c2abe344c4269e9697ea2456df804ce3b
Fra7220 <chantulofrancis4@gmail.com>
2025-12-07 11:22:10 +0200

    jj

--- app/(root)/collections/[collectionId]/page.tsx
+++ app/(root)/collections/[collectionId]/page.tsx
@@ -3,6 +3,10 @@ import { getCollectionDetails } from "@/lib/actions/actions";
 import Image from "next/image";
 import React from "react";
 
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay } from "swiper/modules";
+import "swiper/css";
+
 const CollectionDetails = async ({
   params,
 }: {
@@ -19,12 +23,35 @@ const CollectionDetails = async ({
         alt="collection"
         className="w-full h-[400px] object-cover rounded-xl"
       />
-      <p className="text-heading3-bold text-grey-2">{collectionDetails.title}</p>
-      <p className="text-body-normal text-grey-2 text-center max-w-[900px]">{collectionDetails.description}</p>
-      <div className="flex flex-wrap gap-16 justify-center">
-        {collectionDetails.products.map((product: ProductType) => (
-          <ProductCard key={product._id} product={product} />
-        ))}
+
+      <p className="text-heading3-bold text-grey-2">
+        {collectionDetails.title}
+      </p>
+
+      <p className="text-body-normal text-grey-2 text-center max-w-[900px]">
+        {collectionDetails.description}
+      </p>
+
+      <div className="w-full max-w-[1500px] mt-5">
+        <Swiper
+          modules={[Autoplay]}
+          spaceBetween={30}
+          slidesPerView={1}
+          autoplay={{ delay: 2500, disableOnInteraction: false }}
+          breakpoints={{
+            640: { slidesPerView: 2 },
+            768: { slidesPerView: 3 },
+            1024: { slidesPerView: 4 },
+            1280: { slidesPerView: 6 },
+          }}
+          loop={true}
+        >
+          {collectionDetails.products.map((product: ProductType) => (
+            <SwiperSlide key={product._id}>
+              <ProductCard product={product} />
+            </SwiperSlide>
+          ))}
+        </Swiper>
       </div>
     </div>
   );
@@ -33,4 +60,3 @@ const CollectionDetails = async ({
 export default CollectionDetails;
 
 export const dynamic = "force-dynamic";
-
