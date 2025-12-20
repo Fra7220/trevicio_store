c8848fddb7ca75941c750bf5abe62327362ae3c1
Fra7220 <chantulofrancis4@gmail.com>
2025-12-20 07:08:03 +0200

    collection

--- app/(root)/collections/[collectionId]/page.tsx
+++ app/(root)/collections/[collectionId]/page.tsx
@@ -1,6 +1,3 @@
-export const dynamic = "force-dynamic";
-
-
 import ProductCard from "@/components/ProductCard";
 import { getCollectionDetails } from "@/lib/actions/actions";
 import Image from "next/image";
@@ -13,10 +10,6 @@ const CollectionDetails = async ({
 }) => {
   const collectionDetails = await getCollectionDetails(params.collectionId);
 
-  if (!collectionDetails) {
-    return <p className="text-center py-10">Collection not found.</p>;
-  }
-
   return (
     <div className="px-10 py-5 flex flex-col items-center gap-8">
       <Image
@@ -27,16 +20,17 @@ const CollectionDetails = async ({
         className="w-full h-[400px] object-cover rounded-xl"
       />
       <p className="text-heading3-bold text-grey-2">{collectionDetails.title}</p>
-
-      {Array.isArray(collectionDetails.products) && collectionDetails.products.length > 0 && (
-        <div className="flex flex-wrap gap-16 justify-center">
-          {collectionDetails.products.map((product: ProductType) => (
-            <ProductCard key={product._id} product={product} />
-          ))}
-        </div>
-      )}
+      <p className="text-body-normal text-grey-2 text-center max-w-[900px]">{collectionDetails.description}</p>
+      <div className="flex flex-wrap gap-16 justify-center">
+        {collectionDetails.products.map((product: ProductType) => (
+          <ProductCard key={product._id} product={product} />
+        ))}
+      </div>
     </div>
   );
 };
 
 export default CollectionDetails;
+
+export const dynamic = "force-dynamic";
+
